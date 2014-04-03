d43c98f91b346a3b02461001138aaae17399f010
Tojo Chacko <joey85in@gmail.com>
2014-04-03 23:14:28 +0530

    Pubsub implemented

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//some other constructor
 function insertQuotes(msg) {
 	this.quotedMessage = 'test';
+	//setTimeout to imitate some long operation
 	setTimeout(function() {
 			this.quotedMessage = '"'+msg+'"';
-	}, 4000);	
+			PubSub.publish('msgquoted', {message:this.quotedMessage});
+	}, 3000);	
 }
 
+//some constructor
 function showMessage(msg) {
 	this.message = msg;
 	this.init();
@@ -12,9 +16,12 @@ function showMessage(msg) {
 
 showMessage.prototype = {
        init : function() {
-		      var temp = new insertQuotes(this.message);	
-		      console.log(temp.quotedMessage);
+			var temp = new insertQuotes(this.message);	
+			PubSub.subscribe('msgquoted', function(name, data) {
+					console.log(name);
+					console.log(data);
+					window.document.write(data.message);
+			});
        }
 };
 
-new showMessage('tojo chacko');
